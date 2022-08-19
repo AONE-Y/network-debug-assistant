53adf34d8a28fc17297452f45b19e14f3b2d41b3
anone <1033825262@qq.com>
2022-08-19 21:37:01 +0800

    完成右上部分

---
+++ src/components/MessageReceive.vue
@@ -0,0 +1,79 @@
+<template>
+  <a-layout-header style="background-color:#eae9e9;height: 6%">
+    <a-form-item label="限制" style="height: 100%;margin-left: 1%">
+      <a-select v-model="option.limit" size="mini" style="width: 13%">
+        <a-option value="512k">512 KB</a-option>
+        <a-option value="1m">1 MB</a-option>
+        <a-option value="2m">2 MB</a-option>
+        <a-option value="4m">4 MB</a-option>
+        <a-option value="8m">8 MB</a-option>
+        <a-option value="16m">16 MB</a-option>
+        <a-option value="32m">32 MB</a-option>
+        <a-option value="64m">64 MB</a-option>
+        <a-option value="128m">128 MB</a-option>
+        <a-option value="512m">512 MB</a-option>
+        <a-option value="1g">1 GB</a-option>
+      </a-select>
+      <a-space size="large" style="margin-left: 2%">
+        <a-checkbox v-model="option.hidden">不显示数据</a-checkbox>
+        <a-checkbox v-model="option.receiveMessage">添加接收信息</a-checkbox>
+        <a-checkbox v-model="option.autoSave">自动保存</a-checkbox>
+      </a-space>
+    </a-form-item>
+
+  </a-layout-header>
+  <a-layout-content style="background-color: white;">
+    <a-tabs style="height: 100%;background-color: #eae9e9">
+      <template #extra>
+        <a-button size="mini">清除</a-button>
+        <span>&emsp;</span>
+        <a-button size="mini">保存</a-button>
+        <span>&emsp;</span>
+      </template>
+      <a-tab-pane key="1" title="ASCII">
+        <a-textarea style="height: 87%;position: absolute;width: 100%;display: flex" auto-size disabled
+                    v-model="dataStr"/>
+      </a-tab-pane>
+      <a-tab-pane key="2" title="UTF8">
+        <a-textarea style="height: 87%;position: absolute;width: 100%;display: flex" auto-size disabled/>
+      </a-tab-pane>
+      <a-tab-pane key="3" title="UTF16">
+        <a-textarea style="height: 87%;position: absolute;width: 100%;display: flex" auto-size disabled/>
+      </a-tab-pane>
+      <a-tab-pane key="4" title="UTF32">
+        <a-textarea style="height: 87%;position: absolute;width: 100%;display: flex" auto-size disabled/>
+      </a-tab-pane>
+      <a-tab-pane key="5" title="UTF16-BE">
+        <a-textarea style="height: 87%;position: absolute;width: 100%;display: flex" auto-size disabled/>
+      </a-tab-pane>
+      <a-tab-pane key="6" title="UTF32-BE">
+        <a-textarea style="height: 87%;position: absolute;width: 100%;display: flex" auto-size disabled/>
+      </a-tab-pane>
+      <a-tab-pane key="7" title="HEX">
+        <a-textarea style="height: 87%;position: absolute;width: 100%;display: flex" auto-size disabled/>
+      </a-tab-pane>
+    </a-tabs>
+  </a-layout-content>
+</template>
+
+<script lang="ts" setup>
+import {reactive} from "vue";
+
+const option = reactive({
+  limit: "2m",
+  hidden: false,
+  receiveMessage: false,
+  autoSave: false
+})
+const data = reactive(["123\n", "fdafdafd"])
+let dataStr = "";
+data.forEach((e) => {
+      dataStr+=e;
+    }
+)
+console.log(dataStr);
+</script>
+
+<style scoped>
+
+</style>
---
+++ src/components/SelectionPanel.vue
@@ -0,0 +1,69 @@
+<template>
+  <a-form :model="form" style="height: 70%;width: 100%" @submit="handleSubmit" auto-label-width>
+    <a-card style="width: 100%;height: 100%;padding-top: 15px;">
+      <template #title>
+        <a-form-item field="type" label="类型&emsp;&emsp;">
+          <a-select v-model="form.type" placeholder="Please select ...">
+            <a-option value="tcp-s">TCP服务端</a-option>
+            <a-option value="tcp-c">TCP客户端</a-option>
+            <a-option value="tcp-mc">TCP多并发客户端</a-option>
+            <a-option value="udp-base">UDP基础通讯</a-option>
+            <a-option value="udp-board">UDP广播通讯</a-option>
+            <a-option value="web-s">WebSocket服务端</a-option>
+            <a-option value="web-c">WebSokcet客户端</a-option>
+            <a-option value="web-mc">WebSokcet多并发客户端</a-option>
+          </a-select>
+        </a-form-item>
+      </template>
+      <tcp-s :form="form" v-if="form.type==='tcp-s'"></tcp-s>
+      <tcp-c :form="form" v-if="form.type==='tcp-c'"></tcp-c>
+      <tcp-mc :form="form" v-if="form.type==='tcp-mc'"></tcp-mc>
+      <udp-base :form="form" v-if="form.type==='udp-base'"></udp-base>
+      <udp-board :form="form" v-if="form.type==='udp-board'"></udp-board>
+      <web-s :form="form" v-if="form.type==='web-s'"></web-s>
+      <web-c :form="form" v-if="form.type==='web-c'"></web-c>
+      <web-mc :form="form" v-if="form.type==='web-mc'"></web-mc>
+    </a-card>
+  </a-form>
+  <a-textarea :placeholder="form.info" disabled style="height: 20%;-webkit-text-fill-color:black" auto-size/>
+  <a-button type="primary" size="large">连接</a-button>
+</template>
+
+<script lang="ts" setup>
+import {reactive} from "vue";
+import TcpS from "../components/TcpS.vue";
+import TcpC from "../components/TcpC.vue";
+import TcpMc from "../components/TcpMC.vue";
+import UdpBase from "../components/UdpBase.vue";
+import UdpBoard from "../components/UdpBoard.vue";
+import WebS from "../components/WebS.vue";
+import WebC from "../components/WebC.vue";
+import WebMc from "../components/WebMc.vue";
+
+const form = reactive({
+  type: 'tcp-s',
+  addr: "127.0.0.1",
+  wsAddr: "ws://127.0.0.1:9999",
+  port: 8888,
+  localAddr: "127.0.0.1",
+  localPort: 9999,
+  localPath: '/',
+  addressReuse: 0,
+  keepAlive: 0,
+  boardCast: 0,
+  dataModel: 0,
+  maskProcess: 0,
+  additionalHeaders0: '',
+  additionalHeaders1: '',
+  additionalHeaders2: '',
+  threads: 1,
+  protocols: '提供的文件句柄无效。',
+  info: ''
+})
+const handleSubmit=()=>{}
+
+</script>
+
+<style scoped>
+
+</style>
